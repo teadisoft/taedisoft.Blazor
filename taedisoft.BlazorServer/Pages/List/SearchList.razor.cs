84e12c311c6b910efda6cb257fefa59e1ab71d01
taeho jeong <dotnetfighter@gmail.com>
2022-07-26 04:40:59 +0900

    직렬화/역직렬화

--- taedisoft.BlazorServer/Pages/List/SearchList.razor.cs
+++ taedisoft.BlazorServer/Pages/List/SearchList.razor.cs
@@ -7,7 +7,10 @@ namespace taedisoft.BlazorServer.Pages.List
     public partial class SearchList : ComponentBase
     {
 
-        public IEnumerable<ProjectModel> Projects { get; set; } = Enumerable.Empty<ProjectModel>();
+        //public IEnumerable<ProjectModel> Projects { get; set; } = Enumerable.Empty<ProjectModel>();
+        public List<ProjectModel> Projects { get; set; }
+
+        public string PROJECT_JSON { get; set; } = String.Empty;
 
         /// <summary>
         /// ASP.NET Core Razor 구성 요소 수명 주기
@@ -18,12 +21,15 @@ namespace taedisoft.BlazorServer.Pages.List
         /// <returns></returns>
         protected override async Task OnInitializedAsync()
         {
-            await Task.Run(CreateJsonFile);
+            await Task.Run(JsonSerialize);
         }
 
-        public void CreateJsonFile()
+        /// <summary>
+        /// Serialize (직렬화)
+        /// </summary>
+        public void JsonSerialize()
         {
-            List<ProjectModel> pmList = new List<ProjectModel>()
+            List<ProjectModel> pmList = new()
             {
                 new ProjectModel() { Sabun=1, Name="홍길동", DepartMent="개발팀", Email="gildong@taedisoft.com", Gender =Gender.Male, PhotoPath="111" }
                ,new ProjectModel() { Sabun=2, Name="이순신", DepartMent="총괄팀", Email="sunsin@taedisoft.com", Gender =Gender.Male, PhotoPath="222" }
@@ -31,12 +37,17 @@ namespace taedisoft.BlazorServer.Pages.List
                ,new ProjectModel() { Sabun=4, Name="우영우", DepartMent="법무팀", Email="youyoungyou@taedisoft.com", Gender =Gender.Female, PhotoPath="444" }
             };
 
-            string jsonString = JsonConvert.SerializeObject(pmList);            
+            PROJECT_JSON = JsonConvert.SerializeObject(pmList);            
         }
 
-        public void GetJson()
-        { 
+        /// <summary>
+        /// DeSerialize (역직렬화)
+        /// </summary>
+        public void JsonDeSerialize()
+        {
+            Projects = new List<ProjectModel>();
 
+            Projects = JsonConvert.DeserializeObject<List<ProjectModel>>(PROJECT_JSON);
         }
     }
 }
