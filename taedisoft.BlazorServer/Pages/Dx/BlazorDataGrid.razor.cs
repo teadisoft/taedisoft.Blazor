aa6696b9ba379dd39738cec2a91e1720a10cb21e
taeho jeong <dotnetfighter@gmail.com>
2022-07-28 03:12:19 +0900

    111111111

--- taedisoft.BlazorServer/Pages/Dx/BlazorDataGrid.razor.cs
+++ taedisoft.BlazorServer/Pages/Dx/BlazorDataGrid.razor.cs
@@ -5,25 +5,25 @@ namespace taedisoft.BlazorServer.Pages.Dx
 {
     public partial class BlazorDataGrid : ComponentBase
     {
-        //async Task OnRowRemoving(WeatherForecastModel dataItem)
-        //{
-        //    await ForecastService.Remove(dataItem);
-        //}
-        //async Task OnRowUpdating(WeatherForecastModel dataItem, IDictionary<string, object> newValue)
-        //{
-        //    await ForecastService.Update(dataItem, newValue);
-        //}
-        //async Task OnRowInserting(IDictionary<string, object> newValue)
-        //{
-        //    await ForecastService.Insert(newValue);
-        //}
-        //Task OnInitNewRow(Dictionary<string, object> values)
-        //{
-        //    values.Add("Date", DateTime.Now);
-        //    values.Add("TemperatureC", 13);
-        //    values.Add("Forecast", "Warm");
-        //    values.Add("CloudCover", "Sunny");
-        //    return Task.CompletedTask;
-        //}
+        async Task OnRowRemoving(WeatherForecastModel dataItem)
+        {
+            await ForecastService.Remove(dataItem);
+        }
+        async Task OnRowUpdating(WeatherForecastModel dataItem, IDictionary<string, object> newValue)
+        {
+            await ForecastService.Update(dataItem, newValue);
+        }
+        async Task OnRowInserting(IDictionary<string, object> newValue)
+        {
+            await ForecastService.Insert(newValue);
+        }
+        Task OnInitNewRow(Dictionary<string, object> values)
+        {
+            values.Add("Date", DateTime.Now);
+            values.Add("TemperatureC", 13);
+            values.Add("Forecast", "Warm");
+            values.Add("CloudCover", "Sunny");
+            return Task.CompletedTask;
+        }
     }
 }
